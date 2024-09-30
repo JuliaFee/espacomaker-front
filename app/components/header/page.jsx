abe0366d1ff1bcdb98aa064ecdc13878c2474668
Maria Valonga <maria.valonga@aluno.senai.br>
2024-09-30 14:51:11 -0300

    feat: finalização do css do header

--- app/components/header/page.jsx
+++ app/components/header/page.jsx
@@ -1,21 +1,20 @@
 import { VscTools } from "react-icons/vsc";
 import { CiCalendar } from "react-icons/ci";
-import styles from './header.modules.css'; 
+import styles from './header.module.css'; 
+
 const Header = () => {
     return (
-        <>
         <div className={styles.global}>
             <div className={styles.nav}>
-                <p><span className={styles.senai}>SENAI</span> <span className={styles.lab}>Lab</span></p>
-              </div>
+                <p><span className={styles.senai}>SENAI</span> <span className={styles.lab}>lab</span></p>
+            </div>
             <div className={styles.tools}>
-                    <a href="#"><VscTools className={styles.icon}/></a>
-                    <a href="#"><CiCalendar className={styles.icon}/></a>
-
-
-                </div>
+                <a href="#"><VscTools className={styles.icon}/></a>
+                <a href="#"><CiCalendar className={styles.icon}/></a>
+              
+            </div>
         </div>
-        </>
     );
 }
+
 export default Header;
