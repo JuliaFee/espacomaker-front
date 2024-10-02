5bd20b35e082eb71ea75c4e592da2d737abcac7f
Pedro Santos <pedro.miguel@aluno.senai.br>
2024-10-02 15:14:40 -0300

    feat: finalization page home

--- app/page.jsx
+++ app/page.jsx
@@ -7,42 +7,49 @@ import Footer from "./components/footer/page";
 export default function Home() {
   return (
     <div className={styles.page}>
-      
-      <Header></Header> 
+
+      <Header></Header>
       <div className={styles.imagecontainer}>
         <img className={styles.img} src="bannerSM.png"></img>
       </div>
       <div className={styles.section1}>
         <div className={styles.title2}>
-         <p className={styles.title2}>SERVIÇOS</p>
-         </div>
+          <p className={styles.title2}>SERVIÇOS</p>
+        </div>
 
-         <div className={styles.linkSection}> 
+        <div className={styles.linkSection}>
 
           <div className="reserva-section">
-            <div className="reserva-content">
-            <Link className={styles.link} href={""}>Realizar Reserva</Link>
-
-            </div>
-            <div className="reserva-image">
-              <img src="reserva.png" alt="Ferramentas"></img>
+            <div className="reserva-container">
+              <Link className={styles.link} href={""}>Realizar Reserva</Link>
+              <img className={styles.imgUm} src="reserva.png" alt="Ferramentas"></img>
             </div>
 
-       </div>
-      
-          
-          <Link className={styles.link} href={""}>Acompanhar Andamento</Link>
-          <Link className={styles.link} href={"./ferramentas/"}>Ferramentas</Link>
-         </div>
-         <div className={styles.linkSection}>
-          
-         </div>
+          </div>
+          <div className="reserva-section">
+            <div className="reserva-container">
+              <Link className={styles.link} href={""}>Acompanhar Andamento</Link>
+              <img className={styles.imgDois} src="impressao3D.png" alt="Ferramentas"></img>
+            </div>
+
+          </div>
+          <div className="reserva-section">
+            <div className="reserva-container">
+              <Link className={styles.link} href={"./ferramentas/"}>Ferramentas</Link>
+              <img className={styles.imgTres} src="seletaTools.png" alt="Ferramentas"></img>
+            </div>
+
+          </div>
+        </div>
+        <div className={styles.linkSection}>
+
+        </div>
 
       </div>
 
 
 
-  
+
       <Footer></Footer>
     </div>
   );
