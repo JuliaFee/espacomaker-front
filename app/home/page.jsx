27e0b0a5d4e21b2b0211be7a2df1002945b94748
JuliaFee <julia.silva133@aluno.senai.br>
2024-10-09 10:44:18 -0300

    config do fetch

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,9 +1,13 @@
 "use client";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-import React, { useEffect } from "react";
 import style from "./home.module.css";
 
 const Home = () => {
+  const [ferramentas, setFerramentas] = useState([]);
+  const [deviceType, setDeviceType] = useState("");
+
   const getDeviceType = () => {
     const userAgent = navigator.userAgent;
 
@@ -20,16 +24,36 @@ const Home = () => {
   useEffect(() => {
     const deviceType = getDeviceType();
     document.body.classList.add(deviceType);
+    setDeviceType(deviceType);
 
     return () => {
       document.body.classList.remove(deviceType);
     };
   }, []);
 
+  useEffect(() => {
+    async function fetchFerramentas() {
+      try {
+        console.log("pegando ferramentas");
+        const response = await axios.get("/api/ferramentas");
+        console.log("Response:", response);
+        setFerramentas(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    fetchFerramentas();
+  }, []);
+
   return (
     <div className={style.body}>
       <h1>Hello World !!!</h1>
-      <h3>{getDeviceType()}</h3>
+      <h3>{deviceType}</h3>
+      <ul>
+        {ferramentas.map((ferramenta, index) => (
+          <li key={index}>{ferramenta.nome}</li>
+        ))}
+      </ul>
     </div>
   );
 };
