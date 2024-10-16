af3389cf2808644091d293cd26c5dab3304250a2
Pedro Santos <pedro.miguel@aluno.senai.br>
2024-10-16 10:37:15 -0300

    feat: finalization ferramentas page

--- app/ferramentas/cadastro-ferramentas/page.jsx
+++ app/ferramentas/cadastro-ferramentas/page.jsx
@@ -1,67 +1,74 @@
 "use client";
 import React, { useState } from 'react';
-import styles from './cadastroferramentas.module.css'; 
+import styles from './cadastroferramentas.module.css';
+import Header from "../../components/header/page";
+import Footer from "../../components/footer/page";
+
 
 const FerramentasForm = () => {
-  const [nome, setNome] = useState('');
-  const [descricao, setDescricao] = useState('');
-  const [img, setImg] = useState('');
-  const [statusF, setStatusF] = useState(false);
+    const [nome, setNome] = useState('');
+    const [descricao, setDescricao] = useState('');
+    const [img, setImg] = useState('');
+    // const [statusF, setStatusF] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-    // Lógica para enviar os dados para o backend
-    const novaFerramenta = {
-      nome,
-      descricao,
-      img,
-      statusF
+        // Lógica para enviar os dados para o backend
+        const novaFerramenta = {
+            nome,
+            descricao,
+            img,
+            //statusF
+        };
+
+        console.log('Ferramenta cadastrada:', novaFerramenta);
+        // requisição para o backend 
     };
 
-    console.log('Ferramenta cadastrada:', novaFerramenta);
-    // requisição para o backend 
-  };
+    return (
 
-  return (
-    <div className={styles.container}>
-      <form onSubmit={handleSubmit}>
-        <div className={styles.formGroup}>
-          <label htmlFor="nome">Nome:</label>
-          <input
-            type="text"
-            className={styles.input}
-            id="nome"
-            value={nome}
-            onChange={(e) => setNome(e.target.value)}
-            required
-          />
-        </div>
+        <div className={styles.pageContainer}>
+            <Header></Header>
+            <div className={styles.container}>
+                <form onSubmit={handleSubmit}>
+                    <div className={styles.formGroup}>
+                        <p className={styles.title}>Registrar ferramentas</p>
+                        <label htmlFor="nome" className={styles.label}>Nome:</label>
+                        <input
+                            type="text"
+                            className={styles.input}
+                            id="nome"
+                            value={nome}
+                            onChange={(e) => setNome(e.target.value)}
+                            required
+                        />
+                    </div>
 
-        <div className={styles.formGroup}>
-          <label htmlFor="descricao">Descrição:</label>
-          <input
-            type="text"
-            className={styles.input}
-            id="descricao"
-            value={descricao}
-            onChange={(e) => setDescricao(e.target.value)}
-            required
-          />
-        </div>
+                    <div className={styles.formGroup}>
+                        <label htmlFor="descricao" className={styles.label}>Descrição:</label>
+                        <input
+                            type="text"
+                            className={styles.input}
+                            id="descricao"
+                            value={descricao}
+                            onChange={(e) => setDescricao(e.target.value)}
+                            required
+                        />
+                    </div>
 
-        <div className={styles.formGroup}>
-          <label htmlFor="img">Imagem (URL):</label>
-          <input
-            type="text"
-            className={styles.input}
-            id="img"
-            value={img}
-            onChange={(e) => setImg(e.target.value)}
-          />
-        </div>
+                    <div className={styles.formGroup}>
+                        <label htmlFor="img" className={styles.label}>Imagem (URL):</label>
+                        <input
+                            type="text"
+                            className={styles.input}
+                            id="img"
+                            value={img}
+                            onChange={(e) => setImg(e.target.value)}
+                        />
+                    </div>
 
-        <div className={styles.formGroup}>
+                    {/* <div className={styles.formGroup}>
           <label htmlFor="statusF">Status:</label>
           <input
             type="checkbox"
@@ -70,18 +77,21 @@ const FerramentasForm = () => {
             checked={statusF}
             onChange={(e) => setStatusF(e.target.checked)}
           />
-        </div>
+        </div> */}
 
-        <button
-          type="submit"
-          className={`${styles.button} ${styles.submitButton}`}
-        >
-          Cadastrar Ferramenta
-        </button>
-      </form>
-      {/*  pop-up de sucesso ou erro */}
-    </div>
-  );
+                    <button
+                        type="submit"
+                        className={`${styles.button} ${styles.submitButton}`}
+                    >
+                        Cadastrar
+                    </button>
+                </form>
+                {/*  pop-up de sucesso ou erro */}
+            </div>
+
+            <Footer></Footer>
+        </div>
+    );
 };
 
 export default FerramentasForm;
