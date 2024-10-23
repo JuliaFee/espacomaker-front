57210ceb264712c983b96d1dd132fc3c23b15d6e
Amandamoonchild <amandasilva@aluno.senai.br>
2024-10-23 10:40:07 -0300

    feat: cadastro going

--- app/cadastroADM/page.jsx
+++ app/cadastroADM/page.jsx
@@ -1,124 +1,133 @@
 "use client";
-import React, { useState, useEffect, useRouter } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './cadastroADM.module.css';
 import axios from 'axios';
+// import Header from "./login";
+// import Footer from "./login";
 
 
-const CadastroForm = () => {
+const CadastroADM = () => {
     const [nome, setNome] = useState('');
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
-    const [dados, setDados] = useState('');
+    // const [dados, setDados] = useState('');
     const [tipo, setTipo] = useState('');
-    const router = useRouter();
 
-        const entrar = async () => {
-            router.push(`/loginADM`)
-        };
-    
 
-        useEffect(() => {
-            const fetchData = async () => {
-                try {
-                    const response = await axios.post("http://localhost:000/users");
-                    console.log('Dados recebidos na API:', response.data);
-                    setDados(response.data);
-                } catch (error) {
-                    console.error('Erro ao postar os dados da API:', error);
-                }
-            };
-            fetchData();
-        }, []);
-
-        const handleSubmit = async (e) => {
-            e.preventDefault();
-
-            const novoUsuario = {
-                nome,
-                email,
-                senha,
-                tipo,
-            };
-
-            console.log(novoUsuario);
-
+    useEffect(() => {
+        const fetchData = async () => {
             try {
-                const response = await axios.post("http://localhost:5000/users", novoUsuario);
-                console.log('Usuário cadastrado:', response.data);
-                entrar();
+                const response = await axios.get("http://localhost:5000/users");
+                console.log('Dados recebidos da API:', response.data);
+                console.log('Esta funfando tomaaa macetabdo');
+                setDados(response.data);
             } catch (error) {
-                console.error('Erro ao cadastrar usuário:', error.response ? error.response.data : error.message);
+                console.error('Erro ao obter os dados da API:', error);
+                console.error('teste', error);
             }
-            
+        };
+        fetchData();
+
+    }, []);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        // Crie um objeto com os dados que você deseja enviar
+        const novoUsuario = {
+            nome,
+            email,
+            senha,
+            tipo,
         };
 
-        return (
-            <div className={styles.pageContainer}>
-                <div className={styles.container}>
-                    <form onSubmit={handleSubmit}>
-                        <div className={styles.formGroup}>
-                            <p className={styles.title}>Login</p>
-                            <label htmlFor="nome" className={styles.label}>Nome:</label>
-                            <input
-                                type="text"
-                                className={styles.input}
-                                id="nome"
-                                value={nome}
-                                onChange={(e) => setNome(e.target.value)}
-                                required
-                            />
-                        </div>
+        console.log(novoUsuario); // Para verificar o objeto que será enviado
 
-                        <div className={styles.formGroup}>
-                            <label htmlFor="email" className={styles.label}>Email:</label>
-                            <input
-                                type="text"
-                                className={styles.input}
-                                id="email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                required
-                            />
-                        </div>
+        try {
+            // Envie o objeto como o segundo argumento para a requisição POST
+            const response = await axios.post("http://localhost:5000/users", novoUsuario);
+            console.log('Usuário cadastrado:', response.data);
+            setNome('');
+            setEmail('');
+            setSenha('');
+            setTipo('');
+        } catch (error) {
+            console.error('Erro ao cadastrar usuário:', error);
+        }
 
-                        <div className={styles.formGroup}>
-                            <label htmlFor="senha" className={styles.label}>Senha:</label>
-                            <input
-                                type="password"
-                                className={styles.input}
-                                id="senha"
-                                value={senha}
-                                onChange={(e) => setSenha(e.target.value)}
-                                required
-                            />
-                        </div>
+    };
 
-                        <div className={styles.formGroup}>
-                            <label htmlFor="tipo" className={styles.label}>Tipo:</label>
-                            <select
-                                className={styles.input}
-                                id="tipo"
-                                value={tipo}
-                                onChange={(e) => setTipo(e.target.value)}
-                                required
-                            >
-                                <option value="" disabled>Selecione uma opção</option>
-                                <option value="aluno">Aluno</option>
-                                <option value="docente">Docente</option>
-                            </select>
-                        </div>
 
-                        <button
-                            type="submit"
-                            className={`${styles.button} ${styles.submitButton}`}
-                            onClick={entrar}
+    return (
+
+        <div className={styles.pageContainer}>
+            {/* <Header></Header> */}
+            <div className={styles.container}>
+                <form onSubmit={handleSubmit}>
+                    <div className={styles.formGroup}>
+                        <p className={styles.title}>Login de usuário</p>
+                        <label htmlFor="nome" className={styles.label}>Nome:</label>
+                        <input
+                            type="text"
+                            className={styles.input}
+                            id="nome"
+                            value={nome}
+                            onChange={(e) => setNome(e.target.value)}
+                            required
+                        />
+                    </div>
+
+                    <div className={styles.formGroup}>
+                        <label htmlFor="email" className={styles.label}>Email:</label>
+                        <input
+                            type="text"
+                            className={styles.input}
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                    </div>
+
+                    <div className={styles.formGroup}>
+                        <label htmlFor="senha" className={styles.label}>Senha:</label>
+                        <input
+                            type="text"
+                            className={styles.input}
+                            id="senha"
+                            value={senha}
+                            onChange={(e) => setSenha(e.target.value)}
+                            required
+                        />
+                    </div>
+
+                    <div className={styles.formGroup}>
+                        <label htmlFor="tipo" className={styles.label}>Tipo:</label>
+                        <select
+                            className={styles.input}
+                            id="tipo"
+                            value={tipo}
+                            onChange={(e) => setTipo(e.target.value)}
+                            required
                         >
-                        </button>
-                    </form>
-                </div>
+                            <option value="" disabled>Selecione um tipo</option> {/* Para deixar o campo inicialmente vazio */}
+                            <option value="adm">Admin</option> {/* Opção de administrador */}
+                            <option value="user">Usuário</option> {/* Opção de usuário */}
+                        </select>
+                    </div>
+                    <button
+                        type="submit"
+                        className={`${styles.button} ${styles.submitButton}`}
+                    >
+                        Entrar
+                    </button>
+                </form>
+
             </div>
-        );
-    
+
+            {/* <Footer></Footer> */}
+        </div>
+    );
 };
 
-export default CadastroForm;
+export default CadastroADM;
