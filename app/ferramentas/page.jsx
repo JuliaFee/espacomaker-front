317edfcfd704420021d16f65eca553b586ac4e45
Letícia <leticia.bizaglio@aluno.senai.br>
2024-10-30 11:30:50 -0300

    FERRAMENTAS

--- app/ferramentas/page.jsx
+++ app/ferramentas/page.jsx
@@ -3,29 +3,22 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Header from "../components/header/page";
 import Footer from "../components/footer/page";
-import styles from "./pageferramentas.module.css";
+import style from "./pageferramentas.module.css";
 import { MdOutlineDelete } from "react-icons/md";
-import Link from "next/link";
 import { FaRegEdit } from "react-icons/fa";
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation'; // Adicionando o hook para redirecionar
 
 const Ferramentas = () => {
   const [ferramentas, setFerramentas] = useState([]);
   const [deviceType, setDeviceType] = useState("");
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [tipoUsuario, setTipoUsuario] = useState(""); // Adiciona o tipo de usuário
-  const router = useRouter();
+  const router = useRouter(); // Para redirecionar o usuário
 
   const getDeviceType = () => {
     const userAgent = navigator.userAgent;
     return /android/i.test(userAgent) || (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) ? "Mobile" : "Desktop";
   };
-
-  useEffect(() => {
-    const tipo = localStorage.getItem("tipoUsuario");
-    setTipoUsuario(tipo); // Define o tipo de usuário
-  }, []);
 
   useEffect(() => {
     const deviceType = getDeviceType();
@@ -57,46 +50,35 @@ const Ferramentas = () => {
   const handleDelete = async (id) => {
     try {
       await axios.delete(`http://localhost:5000/ferramentas/${id}`);
-      setFerramentas(ferramentas.filter((ferramenta) => ferramenta.id !== id));
+      setFerramentas(ferramentas.filter((ferramenta) => ferramenta.id !== id)); 
     } catch (error) {
       setError("Erro ao excluir a ferramenta. Tente novamente.");
     }
   };
 
   const handleEdit = (id) => {
-    router.push(`/ferramentas/cadastro-ferramentas?id=${id}`);
+    router.push(`/ferramentas/cadastro-ferramentas?id=${id}`); 
   };
 
   return (
-    <div className={styles.body}>
+    <div className={style.body}>
       <Header />
-      <Link href={"../"} className={styles.back}>Ir para home</Link>
-      <h1 className={styles.h1}>Ferramentas Disponíveis</h1>
-      <h3 className={styles.h3}>Tipo de dispositivo: {deviceType}</h3>
+      <h1 className={style.h1}>Ferramentas Disponíveis</h1>
+      <h3 className={style.h3}>Tipo de dispositivo: {deviceType}</h3>
       {loading ? (
-        <p className={styles.loading}>Carregando...</p>
+        <p className={style.loading}>Carregando...</p>
       ) : (
         <>
-          {error && <p className={styles.error}>{error}</p>}
-          <ul className={styles.container}>
+          {error && <p className={style.error}>{error}</p>}
+          <ul className={style.container}>
             {ferramentas.length > 0 ? (
               ferramentas.map((ferramenta) => (
-                <li key={ferramenta.id} className={styles.card}>
-                  <img src={ferramenta.img} alt={ferramenta.nome} className={styles.imagem} />
-                  <h2 className={styles.title}>{ferramenta.nome}</h2>
+                <li key={ferramenta.id} className={style.card}>
+                  <img src={ferramenta.img} alt={ferramenta.nome} className={style.imagem} />
+                  <h2>{ferramenta.nome}</h2>
                   <p>{ferramenta.descricao}</p>
-                  
-                  {/* Botões condicionais com base no tipo de usuário */}
-                  {tipoUsuario === "adm" && (
-                    <>
-                      <button onClick={() => handleEdit(ferramenta.id)} className={styles.editButton}>
-                        <FaRegEdit />
-                      </button>
-                      <button onClick={() => handleDelete(ferramenta.id)} className={styles.deleteButton}>
-                        <MdOutlineDelete />
-                      </button>
-                    </>
-                  )}
+                  <button onClick={() => handleEdit(ferramenta.id)} className={style.editButton}><FaRegEdit /></button>
+                  <button onClick={() => handleDelete(ferramenta.id)} className={style.deleteButton}><MdOutlineDelete /></button>
                 </li>
               ))
             ) : (
