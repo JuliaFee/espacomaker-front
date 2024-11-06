d2e17b38ed29d7b0cde6ace100768500f0de2b37
leticiabizaglio <123502261+leticiabizaglio@users.noreply.github.com>
2024-11-06 09:38:38 -0300

    alterações

--- app/page.jsx
+++ app/page.jsx
@@ -1,43 +1,44 @@
+"use client"
 import styles from "./page.module.css";
-import Link from "next/link";
+import { useRouter } from 'next/navigation';  
 import Header from "./components/header/page";
 import Footer from "./components/footer/page";
 
-
 export default function Home() {
+  const router = useRouter(); 
+
+  const handleUserClick = () => {
+    localStorage.setItem("tipoUsuario", "user");
+    router.push("/login"); 
+  };
+
+  const handleAdminClick = () => {
+    localStorage.setItem("tipoUsuario", "adm");
+    router.push("/login"); 
+  };
+
   return (
     <div className={styles.page}>
-
-      <Header></Header>
+      <Header />
       <div className={styles.imagecontainer}>
-        <img className={styles.imgBanner} src="bannerSM.png"></img>
-      </div>
-      <div className={styles.section1}>
-        <div className={styles.title2}>
-          <p className={styles.link}>SERVIÇOS</p>
-        </div>
-
         <div className={styles.linkSection}>
-
           <div className="reservaContent">
-            <Link className={styles.link2} href={"./reserva"}>REALIZAR RESERVA</Link>
+            <button className={styles.link2} onClick={handleUserClick}>
+              SOU USUÁRIO
+            </button>
             <img className={styles.img1} src="controlar_andamentos.png" alt="Ferramentas" />
           </div>
 
           <div className="reservaContent">
-            <Link className={styles.link2} href={"./andamento"}>ACOMPANHAR ANDAMENTO</Link>
+            <button className={styles.link2} onClick={handleAdminClick}>
+              SOU ADMINISTRADOR
+            </button>
             <img className={styles.img2} src="reserva.png" alt="Ferramentas" />
           </div>
-        
         </div>
-
       </div>
 
-
-
-
-      <Footer></Footer>
+      <Footer />
     </div>
   );
 }
-
