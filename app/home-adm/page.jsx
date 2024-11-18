d4e3171ae32b2e7b43a7e20c83e20669f4d7efd8
JuliaFee <julia.silva133@aluno.senai.br>
2024-11-18 10:27:28 -0300

    novas pags pra substituir o login caso nn de certo

---
+++ app/home-adm/page.jsx
@@ -0,0 +1,48 @@
+import styles from "./homeadm.module.css";
+import Link from "next/link";
+import Header from "../components/header/page";
+import Footer from "../components/footer/page";
+
+
+export default function Home_adm() {
+  return (
+    <div className={styles.page}>
+
+      <Header></Header>
+      <div className={styles.imagecontainer}>
+        <img className={styles.imgBanner} src="banner2.png"></img>
+      </div>
+      <div className={styles.section1}>
+        <div className={styles.title2}>
+          <p className={styles.link}>SERVIÇOS</p>
+        </div>
+
+        <div className={styles.linkSection}>
+
+          <div className="reservaContent">
+            <Link className={styles.link2} href={"./reserva/reserva-lista"}>ADMNISTRAR RESERVAS</Link>
+          
+          </div>
+
+          <div className="reservaContent">
+            <Link className={styles.link2} href={"./ferramentas/cadastro-ferramentas"}>CADASTRAR FERRAMENTAS</Link>
+            <img className={styles.img1} src="controlar_andamentos.png" alt="Ferramentas" />
+          </div>
+
+          <div className="reservaContent">
+            <Link className={styles.link2} href={"./impressora/cadastro-impressora"}>CADASTRAR IMPRESSORA</Link>
+            <img className={styles.img2} src="reserva.png" alt="Ferramentas" />
+          </div>
+        
+        </div>
+
+      </div>
+
+
+
+
+      <Footer></Footer>
+    </div>
+  );
+}
+
